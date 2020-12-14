ded941f94c59723c2720f4bf7ab2510d84b96625
SergeiAlabugaSDD <SIgnatiev@alabuga.ru>
2020-12-14 17:11:08 +0300

    events created

--- src/app/bus/gameCell/actions.js
+++ src/app/bus/gameCell/actions.js
@@ -7,5 +7,9 @@ export const gameActions = {
   setCanvasColor: createAction(gameCellTypes.SET_CANVAS_COLOR),
   toggleRun: createAction(gameCellTypes.TOGGLE_RUN),
   fillField: createAction(gameCellTypes.FILL_CURRENT_FIELD),
+  clearField: createAction(gameCellTypes.CLEAR_CURRENT_FIELD),
   setTriger: createAction(gameCellTypes.SET_TRIGGER),
+  goOneStep: createAction(gameCellTypes.GO_ONE_STEP),
+  toggleAlive: createAction(gameCellTypes.TOGGLE_ALIVE),
+  setChangedFalse: createAction(gameCellTypes.SET_CHANGED_FALSE),
 };
